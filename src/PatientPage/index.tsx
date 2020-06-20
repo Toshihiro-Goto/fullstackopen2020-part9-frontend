795cd8c68c0e55ab26820a4b6e5be4a5ced78f3f
ToshihiroGoto <57031455+Toshihiro-Goto@users.noreply.github.com>
2020-06-20 21:32:27 +0900

    Add a page that shows patient's full information.
    Fetch the data from the enpoint created in the previous exercise.
    Do not fetch the information if it already is in the app state.
    
    9.17: patientor, step2

---
+++ src/PatientPage/index.tsx
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Icon, SemanticICONS } from "semantic-ui-react";
+import axios from "axios";
+import { useStateValue } from "../state";
+import { Patient, Gender } from "../types";
+import { apiBaseUrl } from "../constants";
+
+const PatientPage: React.FC = () => {
+  const [{ patients }, dispatch] = useStateValue();
+  const [patient, setPatient] = useState<Patient>();
+  const { id } = useParams<{ id: string }>();
+  useEffect(() => {
+    const p = Object.values(patients).find((p) => p.id === id);
+    if (p != null) return setPatient(p);
+
+    const fetchPatient = async () => {
+      try {
+        const { data: patientFromApi } = await axios.get<Patient>(
+          `${apiBaseUrl}/patients/${id}`
+        );
+        setPatient(patientFromApi);
+        dispatch({ type: "ADD_PATIENT", payload: patientFromApi });
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    fetchPatient();
+  }, [id, patients, dispatch]);
+
+  const mapToIconName = (gender: Gender): SemanticICONS => {
+    enum GenderIconName {
+      female = "woman",
+      male = "man",
+      other = "other gender",
+    }
+    return GenderIconName[gender];
+  };
+
+  return patient ? (
+    <>
+      <h1>
+        {patient.name}
+        {<Icon name={mapToIconName(patient.gender)}></Icon>}
+      </h1>
+      <p>ssn:{patient.ssn}</p>
+      <p>occupation:{patient.occupation}</p>
+    </>
+  ) : null;
+};
+
+export default PatientPage;
