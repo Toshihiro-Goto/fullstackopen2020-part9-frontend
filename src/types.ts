951c98d6b0afd73ef07798c95645df93e774a5ab
ToshihiroGoto <57031455+Toshihiro-Goto@users.noreply.github.com>
2020-07-03 18:58:05 +0900

    Add entry types.
    -
    
    Full entries

--- src/types.ts
+++ src/types.ts
@@ -18,3 +18,41 @@ export interface Patient {
   ssn?: string;
   dateOfBirth?: string;
 }
+
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis['code']>;
+}
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+interface HospitalEntry {
+  type: "Hospital";
+  discharge: {
+    date: Date;
+    criteria: string;
+  };
+}
+
+interface OccupationalHealthcareEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
